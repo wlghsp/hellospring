cfd63ecf15eac0dc1bc21c62bb72a6a2900f54f9
CHOIJIHO <wlghsp@gmail.com>
2024-06-29 23:47:09 +0900

    PaymentService 개발 (3)

---
+++ src/main/java/tobyspring/hellospring/ExRateData.java
@@ -0,0 +1,14 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+/**
+ * @JsonIgnoreProperties(ignoreUnknown=true)
+ * json 안에 없는게 있어도 무시함
+ */
+@JsonIgnoreProperties(ignoreUnknown=true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+}
