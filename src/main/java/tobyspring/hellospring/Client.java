eed8c5686513909c4c0553f163ccd21a3c7d211b
CHOIJIHO <wlghsp@gmail.com>
2024-06-30 17:40:46 +0900

    상속을 통한 확장

---
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -0,0 +1,13 @@
+package tobyspring.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        PaymentService paymentService = new SimpleExRatePaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+}
