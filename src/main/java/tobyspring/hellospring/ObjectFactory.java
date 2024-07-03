ba579ba313b91c4a28a3e37718202638b03d0d8d
JIHO CHOI <wlghsp@gmail.com>
2024-07-03 20:54:06 +0900

    스프링 컨테이너와 의존관계 주입

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,11 +1,21 @@
 package tobyspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 구성정보 (Configuration): 빈 클래스, 의존 관계
+ */
+@Configuration
 public class ObjectFactory {
+
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
-    private ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new SimpleExRateProvider();
     }
 }
