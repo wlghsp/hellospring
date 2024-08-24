048f384bb4487108325e182b823da77ae436dacb
JIHO CHOI <wlghsp@gmail.com>
2024-08-24 15:53:38 +0900

    애플리케이션 서비스 도입

--- src/main/java/tobyspring/hellospring/DataConfig.java
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -11,7 +11,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import tobyspring.hellospring.data.OrderRepository;
 
 import javax.sql.DataSource;
 
@@ -47,8 +46,5 @@ public class DataConfig {
         return new JpaTransactionManager(emf);
     }
 
-    @Bean
-    public OrderRepository orderRepository() {
-        return new OrderRepository();
-    }
+
 }
