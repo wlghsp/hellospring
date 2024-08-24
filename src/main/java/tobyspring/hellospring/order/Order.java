e611089273c578195221fcf2db08958b3e98d5ec
JIHO CHOI <wlghsp@gmail.com>
2024-08-24 16:14:44 +0900

    기술에 독립적인 애플리케이션 서비스

--- src/main/java/tobyspring/hellospring/order/Order.java
+++ src/main/java/tobyspring/hellospring/order/Order.java
@@ -1,18 +1,11 @@
 package tobyspring.hellospring.order;
 
-import jakarta.persistence.*;
-
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
 public class Order {
 
-    @Id
-    @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
