fffccfa9c381498cef3869bad20c96845b67adf0
JIHO CHOI <wlghsp@gmail.com>
2024-08-25 17:06:05 +0900

    트랜잭션 테스트

--- src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
@@ -4,13 +4,18 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tobyspring.hellospring.OrderConfig;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = OrderConfig.class)
@@ -19,11 +24,42 @@ class OrderServiceSpringTest {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
     void createOrder() {
         // var 타입 추론
         var order = orderService.createOrder("0100", BigDecimal.ONE);
 
         assertThat(order.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void crateOrders() {
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0200", BigDecimal.ONE),
+                new OrderReq("0201", BigDecimal.TWO)
+        );
+
+        var orders = orderService.createOrders(orderReqs);
+
+        Assertions.assertThat(orders).hasSize(2);
+        orders.forEach(order -> assertThat(order.getId()).isGreaterThan(0));
+    }
+
+    @Test
+    void crateDuplicatedOrders() {
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0300", BigDecimal.ONE),
+                new OrderReq("0300", BigDecimal.TWO)
+        );
+
+        assertThatThrownBy(() -> orderService.createOrders(orderReqs))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+        JdbcClient client = JdbcClient.create(dataSource);
+        var count = client.sql("select count(*) from orders where no = '0300'").query(Long.class).single();
+        assertThat(count).isEqualTo(0);
     }
 }
